fa5656ad26a2975cb62e898e825dc3ad9412d79b
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-03-26 14:30:05 -0300

    Create If.R

---
+++ codigos/If.R
@@ -0,0 +1,23 @@
+#Controle de fluxo
+
+# if(condição){}
+#   código...
+# }
+
+
+#Exemplo - Controle de fluxo
+
+x = 10
+if (x > 0){
+  print("Número Positivo")
+}
+
+
+nota = 4
+if (nota >=7){
+  print("Aprovado")
+}else if (nota > 5 && nota < 7){
+  print("Recuperação")
+}else{
+  print("Reprovado")
+}
