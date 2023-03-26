de6183d5fc9c3b1bf49edfbb17ec587f272d4123
Thiago Alberto <71457360+Thiago1alberto@users.noreply.github.com>
2023-03-26 14:29:40 -0300

    Create Loops.R

---
+++ codigos/Loops.R
@@ -0,0 +1,30 @@
+#Loops
+
+#For
+
+# for (valor in sequencia){
+#   código...
+# }
+
+?seq
+
+#Exemplo for
+for (i in seq(12)){
+  print(i)
+}
+
+
+#While
+
+# while(condição){
+#   código...
+# }
+
+
+#Exemplo while
+
+i <- 0
+while(i <= 10){
+  print(i)
+  i = i+1
+}
